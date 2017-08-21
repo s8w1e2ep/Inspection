95a196f30c125670f623f1ff58accf3bdf391bd4
s8w1e2ep <ha9mv8c@gmail.com>
2017-08-21 20:11:32 +0800

    Add all statistic view actions to controller

---
+++ InspectionWeb/Controllers/StatisticController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InspectionWeb.Controllers
+{
+    public class StatisticController : Controller
+    {
+        public StatisticController()
+        {
+        }
+
+        // GET: /Statistic
+        public ActionResult Index()
+        {
+            return View("ExhibitionItem");
+        }
+
+        // GET: /Statistic/Exhibition
+        public ActionResult Exhibition()
+        {
+            return View();
+        }
+
+        // GET: /Statistic/ExhibitionItem
+        public ActionResult ExhibitionItem()
+        {
+            return View();
+        }
+
+        // GET: /Statistic/Facility
+        public ActionResult Facility()
+        {
+            return View();
+        }
+
+        // GET: /Statistic/RankItem
+        public ActionResult RankItem()
+        {
+            return View();
+        }
+
+        // GET: /Statistic/RankRecord
+        public ActionResult RankReport()
+        {
+            return View();
+        }
+
+
+    }
+}
