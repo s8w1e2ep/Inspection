ded20a564d6311301298138e78bac61a5f59676f
s8w1e2ep <ha9mv8c@gmail.com>
2017-08-21 20:09:26 +0800

    Add rank item view

---
+++ InspectionWeb/Scripts/rankItem.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $('#fieldTable').DataTable({
+        "paging": false,
+        "info": false,
+        "searching": false,
+        "columnDefs": [
+            { "targets": 0, "width": "10%", "orderable": true },
+            { "targets": 1, "width": "10%", "orderable": true },
+            { "targets": 2, "width": "15%", "orderable": true },
+            { "targets": 3, "width": "15%", "orderable": true },
+            { "targets": 4, "width": "15%", "orderable": true },
+            { "targets": 5, "width": "15%", "orderable": true },
+            { "targets": 6, "width": "20%", "orderable": false }
+        ],
+    });
+});
