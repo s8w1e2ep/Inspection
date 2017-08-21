ce26bffac6fc9a9b989e6aaa0f706cca4cfc48f7
s8w1e2ep <ha9mv8c@gmail.com>
2017-08-21 20:09:45 +0800

    Add rank report view

---
+++ InspectionWeb/Scripts/rankReport.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $('#fieldTable').DataTable({
+        "paging": false,
+        "info": false,
+        "searching": false,
+        "columnDefs": [
+            { "targets": 0, "width": "20%", "orderable": true },
+            { "targets": 1, "width": "20%", "orderable": true },
+            { "targets": 2, "width": "40%", "orderable": true },
+            { "targets": 3, "width": "20%", "orderable": false }
+        ],
+    });
+});
