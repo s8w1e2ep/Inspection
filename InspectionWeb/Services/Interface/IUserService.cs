e383e67afad550fdc91a9ec7e3fe70f792f28dd6
s8w1e2ep <ha9mv8c@gmail.com>
2017-08-23 23:32:46 +0800

    Add user service

---
+++ InspectionWeb/Services/Interface/IUserService.cs
@@ -0,0 +1,27 @@
+﻿using InspectionWeb.Services.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InspectionWeb.Service.Interface
+{
+    public interface IUserService
+    {
+        //IResult Create(user instance);
+
+        //IResult Create(string email, string password);
+        //IResult Update(user instance);
+        //IResult Update(user instance, string propertyName, object value);
+        //IResult Delete(string userID);
+        //IResult changePassword(string userId, string oldPassword, string newPassword);
+
+        //user Login(string email, string password);
+        //bool IsExists(string userID);
+        //user GetByID(string userID);
+        //user GetByEmail(string email);
+        //IEnumerable<user> GetAll();
+    }
+}
