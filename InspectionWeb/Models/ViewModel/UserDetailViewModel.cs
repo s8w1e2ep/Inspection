5d842a8d04f118d5aa3e35f8190b066d729d0316
s8w1e2ep <ha9mv8c@gmail.com>
2017-08-29 00:13:36 +0800

    Add ViewModel for interact with model

---
+++ InspectionWeb/Models/ViewModel/UserDetailViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace InspectionWeb.Models.ViewModel
+{
+    public class UserDetailViewModel
+    {
+        public string userId { get; set; }
+        public string userCode { get; set; }
+        public string groupId { get; set; }
+        public string password { get; set; }
+        public string name { get; set; }
+        public string picture { get; set; }
+        public string agent { get; set; }
+        public string email { get; set; }
+        public string tel { get; set; }
+        public Nullable<short> active { get; set; }
+        public short isDelete { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}")]
+        public Nullable<DateTime> createTime { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}")]
+        public Nullable<DateTime> lastUpdateTime { get; set; }
+    }
+}
