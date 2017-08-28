0029673429b4a4838b66ecab2f0d4ae105c5e9a8
layan <gino63o7@gmail.com>
2017-08-28 17:34:47 +0800

    add Query.js and some comment

---
+++ InspectionWeb/Scripts/Query.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+
+    $('#startDatePicker, #endDatePicker').daterangepicker({
+        singleDatePicker: true,
+        format: 'YYYY/MM/DD'
+    }).on('apply.daterangepicker', function (ev, picker) {
+        // todo
+        //beaconAlarmDateStart = picker.startDate.format('YYYY/MM/DD');
+        //beaconAlarmDateEnd = picker.endDate.format('YYYY/MM/DD');
+        //console.log(scheduleDateStart);
+        //console.log(scheduleDateEnd);
+        });
+
+
+    $('#ReportList').DataTable({
+        "paging": false,
+        "info": false,
+        "searching": false,
+        "columnDefs": [
+            { "targets": 0, "width": "15%", "orderable": true },
+            { "targets": 1, "width": "15%", "orderable": true },
+            { "targets": 2, "width": "15%", "orderable": true },
+            { "targets": 3, "width": "15%", "orderable": true },
+            { "targets": 4, "width": "20%", "orderable": true },
+            { "targets": 5, "width": "10%", "orderable": true },
+            { "targets": 6, "width": "10%", "orderable": false }
+        ],
+    });
+
+    // single Date picker
+});
