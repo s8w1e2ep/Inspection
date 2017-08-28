a066e5f62f8c73222bae858c805caf7acceb83a2
s8w1e2ep <ha9mv8c@gmail.com>
2017-08-29 00:15:01 +0800

    Add id-generator and encrypt method

---
+++ InspectionWeb/Models/Misc/IdGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace InspectionWeb.Models.Misc
+{
+    public class IdGenerator
+    {
+        private string GetUniqID()
+        {
+            var ts = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0));
+            double t = ts.TotalMilliseconds / 1000;
+
+            int a = (int)Math.Floor(t);
+            int b = (int)((t - Math.Floor(t)) * 1000000);
+
+            return a.ToString("x8") + b.ToString("x5");
+        }
+
+
+        public string GetUserNewID()
+        {
+            string id = "user_" + this.GetUniqID().Replace(",", "_");
+            return id;
+        }
+
+        public string GetFieldNewID()
+        {
+            string id = "field_" + this.GetUniqID().Replace(",", "_");
+            return id;
+        }
+
+        public string GetBeaconNewID()
+        {
+            string id = "beacon_" + this.GetUniqID().Replace(",", "_");
+            return id;
+        }
+
+        public string GetParkingblockNewID()
+        {
+            string id = "parkingBlock_" + this.GetUniqID().Replace(",", "_");
+            return id;
+        }
+
+        public string GetID(string Header)
+        {
+            string id = Header + "_" + this.GetUniqID().Replace(",", "_");
+            return id;
+        }
+    }
+}
