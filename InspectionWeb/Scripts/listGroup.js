a292bfbc8c8a7e9aae0843212ce811a729beca42
s8w1e2ep <ha9mv8c@gmail.com>
2017-08-29 23:55:28 +0800

    Add deleteUserGroup method

--- InspectionWeb/Scripts/listGroup.js
+++ InspectionWeb/Scripts/listGroup.js
@@ -1,5 +1,22 @@
-﻿$(document).ready(function () {
-    $('#userTable').DataTable({
+﻿var toDeleteGroupId;
+
+function deleteGroup() {
+    $.ajax({
+        type: "DELETE",
+        url: 'DeleteGroup',
+        data: $.param({ "groupId": toDeleteGroupId })
+    }).done(function (msg) {
+        $("#" + toDeleteGroupId).remove();
+    });
+}
+
+// 設定刪除的 groupId
+function setId(groupId) {
+    toDeleteGroupId = groupId;
+}
+
+$(document).ready(function () {
+    $('#groupTable').DataTable({
         "paging": false,
         "info": false,
         "searching": true,
@@ -13,13 +30,11 @@
             { "targets": 6, "width": "15%", "orderable": true },
             { "targets": 7, "width": "15%", "orderable": true },
             { "targets": 8, "width": "10%", "orderable": false }
-        ],
+        ]
     });
 
-    $('#myModal .modal-footer button').on('click', function (e) {
-        var $target = $(e.target);
-        if ($target.index() == 1) {
-            deleteUser();
-        }
+    // delete dialog 確定事件
+    $('#modalYes').click(function () {
+        deleteGroup();
     });
 });
