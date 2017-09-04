970e429d269f480042b29e8369ab4925095e10e5
layan <gino63o7@gmail.com>
2017-09-05 03:17:58 +0800

    add quicksolution view

---
+++ InspectionWeb/Controllers/QuickSolutionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InspectionWeb.Controllers
+{
+    public class QuickSolutionController : Controller
+    {
+        public ActionResult Add()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
