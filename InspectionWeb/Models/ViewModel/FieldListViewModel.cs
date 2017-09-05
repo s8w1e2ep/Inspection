4b08cb2b3ad5ecda188652ddec66ad82213aa9b3
iamkai <as23041248@gmail.com>
2017-09-06 01:41:30 +0800

    [Information] [ListField] Add FiedAddViewModel for ListFieldView accessing database.

---
+++ InspectionWeb/Models/ViewModel/FieldListViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InspectionWeb.Models.ViewModel
+{
+    public class FieldListViewModel
+    {
+        public string FieldId { get; set; }
+
+        public string FieldName { get; set; }
+
+        public string Version { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}")]
+        public DateTime CreateTime { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}")]
+        public DateTime LastUpdateTime { get; set; }
+
+    }
+}
