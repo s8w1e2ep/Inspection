759a4d5e8e61080ab4b6385341b02fb2d4d94cf5
iamkai <u0251074@nkfust.edu.tw>
2017-09-12 19:32:49 +0800

    [system][softwareVesrion] Finish all about Edit, List software version.

---
+++ InspectionWeb/Models/ViewModel/SoftwareVersionListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InspectionWeb.Models.ViewModel
+{
+    public class SoftwareVersionListViewModel
+    {
+        public string SoftwareId;
+        public string SoftwareCode;
+        public string SoftwareName;
+        public string Version;
+        public DateTime? CreateTime;
+        public DateTime? LastUpdateTime;
+    }
+}
