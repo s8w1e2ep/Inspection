352999ddc2930ed9a9d064785336e07b1c772726
iamkai <u0251074@nkfust.edu.tw>
2017-09-12 16:07:01 +0800

    [System][softVersion] Add softwareVersion controller.

---
+++ InspectionWeb/Controllers/SoftwareVersionController.cs
@@ -0,0 +1,24 @@
+﻿using InspectionWeb.Models;
+using InspectionWeb.Models.ViewModel;
+using InspectionWeb.Services.Interface;
+using InspectionWeb.Services.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InspectionWeb.Controllers
+{
+    public class SoftwareVersionController : Controller
+    {
+
+        public SoftwareVersionController()
+        {
+        }
+
+       
+
+       
+    }
+}
