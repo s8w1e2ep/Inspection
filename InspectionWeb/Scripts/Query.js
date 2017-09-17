36896ea199c787d4111c1d5df43af36db44786f3
Reack <gon5483@gmail.com>
2017-09-17 20:35:08 +0800

    Update from dev

--- InspectionWeb/Scripts/Query.js
+++ InspectionWeb/Scripts/Query.js
@@ -1,7 +1,9 @@
-﻿$(function () {
-    $(".select2").select2();
+﻿var startDate = null;
+var endDate = null;
 
-    $('#ReportList_Item, #ReportList_Experience, #ReportList_Other').DataTable({
+$(document).ready(function () {
+
+    $('#itemTable').DataTable({
         "paging": false,
         "info": false,
         "searching": true,
@@ -13,28 +15,124 @@
             { "targets": 4, "width": "20%", "orderable": true },
             { "targets": 5, "width": "10%", "orderable": true },
             { "targets": 6, "width": "10%", "orderable": false }
-        ],
+        ]
     });
-    //select date and time
-    $('#dateAndTime1, #dateAndTime2').daterangepicker({
-        timePicker: true,
-        timePickerSeconds: true,
-        singleDatePicker: true,
-        timePickerIncrement: 1,
-        format: 'YYYY/MM/DD HH:mm:ss',
-        locale:{
-            applyLabel: '確定',
-            cancelLabel: '取消',
-            fromLabel: '從',
-            toLabel: '到',
-            weekLabel: 'W',
-            customRangeLabel: 'Custom Range',
-            daysOfWeek: ["日", "一", "二", "三", "四", "五", "六"],
-            monthNames: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
-        }
+    $('#expTable').DataTable({
+        "paging": false,
+        "info": false,
+        "searching": true,
+        "columnDefs": [
+            { "targets": 0, "width": "15%", "orderable": true },
+            { "targets": 1, "width": "15%", "orderable": true },
+            { "targets": 2, "width": "15%", "orderable": true },
+            { "targets": 3, "width": "15%", "orderable": true },
+            { "targets": 4, "width": "20%", "orderable": true },
+            { "targets": 5, "width": "10%", "orderable": true },
+            { "targets": 6, "width": "10%", "orderable": false }
+        ]
+    });
+    $('#otherTable').DataTable({
+        "paging": false,
+        "info": false,
+        "searching": true,
+        "columnDefs": [
+            { "targets": 0, "width": "15%", "orderable": true },
+            { "targets": 1, "width": "25%", "orderable": true },
+            { "targets": 2, "width": "20%", "orderable": true },
+            { "targets": 3, "width": "20%", "orderable": true },
+            { "targets": 4, "width": "10%", "orderable": true },
+            { "targets": 5, "width": "10%", "orderable": false }
+
+        ]
     });
 
-    
-
-    
+    // single Date picker
+    $('#startTime, #endTime').datepicker({
+        language: 'zh-TW',
+        format: 'yyyy/mm/dd'
+    }).on('changeDate', function (ev) {
+        startDate = $('#startTime').val();
+        endDate = $('#endTime').val();
+        $('.datepicker').hide();
+    });
 });
+
+function query() {
+
+    if (startDate === null || startDate === "") {
+        alert('請選擇開始日期!');
+    }
+    else if (endDate === null || endDate === "") {
+        alert('請選擇結束日期!');
+    }
+    else if (endDate < startDate) {
+        alert('結束日期不能比開始日期早');
+    }
+    else
+    {
+        setInspectTable('item');
+        setInspectTable('exp');
+        setInspectTable('other');
+    }
+
+    
+}
+function setInspectTable(type) {
+   
+    $.post(
+        '/ReportJob/GetQuery',
+        {
+            sourceId: $('#reportSource').val(),
+            abnormalId: $('#abnormal').val(),
+            isClose: $('#Close').val(),
+            startDate: startDate,
+            endDate: endDate,
+            type: type
+        },
+        function (data) {
+            if (data.length !== 0) {
+                var str;
+
+                $('#' + type + 'ErrDiv').css('display', 'none');
+                $('#' + type + 'Table').DataTable().clear().draw();
+                str = '<a href="' + 'ItemDetailedData' + '" class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-menu-hamburger"></i></a>';
+                
+                for (var i = 0; i < data.length; i++){
+                    str = '<a href="ItemDetailedData/' + data[i].recordId + '" class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-menu-hamburger"></i></a>';
+                    if (type !== 'other')
+                    {
+                        $('#' + type + 'Table').DataTable().row.add([
+                            data[i].sourceName,
+                            data[i].roomName,
+                            data[i].itemName,
+                            data[i].happenedTime,
+                            data[i].fixDate,
+                            data[i].isClose,
+                            str
+                        ]).draw();
+                    }
+                    else
+                    {
+                        $('#otherTable').DataTable().row.add([
+                            data[i].sourceName,
+                            data[i].itemName,
+                            data[i].happenedTime,
+                            data[i].fixDate,
+                            data[i].isClose,
+                            str
+                        ]).draw();
+                    }
+                }
+            }
+            else
+            {
+                $('#' + type + 'Table').DataTable().clear().draw();
+                $('#' + type + 'ErrDiv').css('display', 'block');
+            }
+        },
+        'json'
+    ).fail(function () {
+        //console.log(e);
+        alert("抱歉! 伺服器無法接受您的查詢!");
+    });
+}
